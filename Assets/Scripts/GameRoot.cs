f4d3644d82371d3cbc546342d25979c3f95fae8d
AIR540\Shingo <sin555913@gmail.com>
2018-01-04 23:13:19 +0900

    no message

---
+++ Assets/Scripts/GameRoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameRoot : MonoBehaviour {
+    [SerializeField] GameObject button = null;  // ボタン
+    [SerializeField] GameObject text = null;    // テキスト
+
+    private int count = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // ボタン
+    public void OnPressButton() {
+        // カウント
+        count++;
+        // 表示
+        text.GetComponent<Text>().text = count.ToString();
+    }
+}
